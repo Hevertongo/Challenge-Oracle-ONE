f2f2b5088f162a3842964818da65b53d50820cd2
Heverton L <heverton.go@gmail.com>
2022-08-11 15:55:31 -0300

    Botão copiar agora esconde, aviso e imagem de mensagem não encontrada agora ocultam quando há mensagem, e reaparecem quando não há mensagem

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Variáveis
 var botao1 = document.querySelector('button.botao1');
 var botao2 = document.querySelector('button.botao2');
-var botaoCopia = document.getElementById('botaoCopia')
-
+var botaoCopia = document.getElementById('botaoCopia');
+var entrada = document.getElementById('entrada');
 
 // Interações
 botao1.onclick = criptografa;
@@ -13,7 +13,12 @@ botaoCopia.onclick = copiaTexto;
 // Funções
 function criptografa() {
 	// body...
-	alert("criptografa");
+	// alert("criptografa");
+	if (entrada.value.length == 0) {
+		erroAreaResultado();
+	} else {
+		limpaAreaResultado();
+	}
 }
 
 function descriptografa() {
@@ -23,4 +28,16 @@ function descriptografa() {
 
 function copiaTexto() {
 	alert("copiaTexto");
+}
+
+function limpaAreaResultado () {
+	document.getElementById('msgNone').innerHTML = '<h2 id="msgNone" class="naoEncontrado"></h2>';
+	document.getElementById('imgLupa').style.display = 'none';
+	document.getElementById('areaBotao').innerHTML = '<button id="botaoCopia" class="botao2"><h4>Copiar</h4></button>';
+}
+
+function erroAreaResultado () {
+	document.getElementById('imgLupa').style.display = '';
+	document.getElementById('msgNone').innerHTML = '<h2 id="msgNone" class="naoEncontrado">Nenhuma mensagem encontrada</h2>';
+
 }
